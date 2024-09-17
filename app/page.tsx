31db22a9dcc0b011f3a2e6e2909c629ee6376360
jduran <jduran@gen-it.com.ar>
2024-09-17 20:20:24 -0300

    add description in home

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,20 @@ export default function Home() {
     <div>
       <Navbar />
       <Profile />
+
+      <div className="mt-[40px] text-[#d1d5db]">
+        <p>
+          I’m a fullstack developer specializing in building scalable applications with Node.js and TypeScript. Throughout my career, I’ve worked on projects at various stages, from the ground up to production, as well as maintaining existing projects, always aiming for efficient and robust solutions.
+        </p>
+
+        <p className="mt-5">
+          Currently, I’m studying engineering and I enjoy constantly learning new technologies and strengthening my knowledge. My focus is on improving both my technical skills and contributing to the success of the projects I’m involved in.
+        </p>
+
+        <p className="mt-5">
+          If you’d like to collaborate or just connect, feel free to reach out via email or any of my social platforms.
+        </p>
+      </div>
     </div>
   );
 }
