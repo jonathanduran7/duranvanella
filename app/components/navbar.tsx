0cc201f80447e5b56e7fd9a3f306d6ea7677d399
jduran <jduran@gen-it.com.ar>
2024-09-17 19:38:03 -0300

    add navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,8 +1,10 @@
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
 export default function Navbar() {
   return (
     <div className="w-full flex justify-between">
       <ul className="flex gap-4">
-        <li className="cursor-pointer">
+        <li className="cursor-pointer underline font-bold">
           home
         </li>
         <li className="cursor-pointer">
@@ -13,8 +15,13 @@ export default function Navbar() {
         </li>
       </ul>
 
-      <div>
-        right
+      <div className='flex gap-3'>
+        <div className='cursor-pointer'>
+          <p>es/en</p>
+        </div>
+        <div className='cursor-pointer'>
+          <WbSunnyIcon />
+        </div>
       </div>
     </div >
   );
