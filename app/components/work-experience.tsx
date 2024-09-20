3fd7fe072269660d90add6675c0ab3c248f31d1c
jonathanduran7 <joniduran624@gmail.com>
2024-09-19 21:01:39 -0300

    add experience

--- app/components/work-experience.tsx
+++ app/components/work-experience.tsx
@@ -9,7 +9,7 @@ export default function WorkExperience() {
         <p className="text-gray-dark text-xs">2021 - PRESENT</p>
         <p>Software Developer - GenIT</p>
         <p className="text text-gray">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+          I work with technologies such as React, Angular, Node.js (Adonis/Express), and databases including MySQL and MongoDB. My responsibilities encompass both back-end and front-end development, serving a diverse range of clients.
         </p>
       </div>
     </div>
