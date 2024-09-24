2c4b193db7d1df0f5d58720572aa054cf4fe4332
jonathanduran7 <joniduran624@gmail.com>
2024-09-24 20:59:37 -0300

    modify experiencie

--- app/components/work-experience.tsx
+++ app/components/work-experience.tsx
@@ -9,7 +9,7 @@ export default function WorkExperience() {
         <p className="dark:text-gray-dark text-xs text-black">2021 - PRESENT</p>
         <p className="dark:text-white text-black">Software Developer - GenIT</p>
         <p className="text dark:text-gray text-black">
-          I work with technologies such as React, Angular, Node.js (Adonis/Express), and databases including MySQL and MongoDB. My responsibilities encompass both back-end and front-end development, serving a diverse range of clients.
+          I work with technologies such as React, Angular, Node.js (Adonis/Express/NestJs), and databases including MySQL and MongoDB. My responsibilities encompass both back-end and front-end development, serving a diverse range of clients.
         </p>
       </div>
     </div>
