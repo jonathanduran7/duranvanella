770ca8b3e3de39313fe9a0e53db1e270af816692
jonathanduran7 <joniduran624@gmail.com>
2024-09-24 20:59:02 -0300

    add ref and scroll function

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useRef } from 'react'
 import Navbar from "./ui/navbar";
 import Profile from "./components/profile";
 import Description from "./components/description";
@@ -6,13 +9,22 @@ import Footer from "./components/footer";
 import Contact from "./components/contact";
 
 export default function Home() {
+
+  const contactRef = useRef<HTMLElement | null>(null)
+
+  const scrollToContact = () => {
+    if (contactRef) {
+      contactRef?.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="bg-background text-dark dark:bg-darkBackground dark:text-darkForeground paddingPage min-h-screen">
       <Navbar />
       <Profile />
-      <Description />
+      <Description scroll={scrollToContact} />
       <WorkExperience />
-      <Contact />
+      <Contact refElement={contactRef} />
       <Footer />
     </div>
   );
