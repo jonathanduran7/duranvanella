a4f3813b4edcd622f3c2871612eea1acbbb5b72e
jonathanduran7 <joniduran624@gmail.com>
2024-10-22 23:11:44 -0300

    add type

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,15 +3,15 @@ import { getPostData } from '../../lib/posts';
 
 
 export default async function Post({ params }: { params: { id: string } }) {
-  const postData = await getPostData(params.id);
+  const postData = await getPostData(params.id) as any;
   return (
     <div className="bg-background text-dark dark:bg-darkBackground dark:text-darkForeground paddingPage min-h-screen">
       <Navbar />
       <article className='prose mt-5'>
-        <h1>{postData.title}</h1>
+        <h1>{postData?.title}</h1>
 
         <div className='mt-5 mb-5'>
-          <p>{postData.date}</p>
+          <p>{postData?.date}</p>
         </div>
 
         <div dangerouslySetInnerHTML={{ __html: postData.content }} />
