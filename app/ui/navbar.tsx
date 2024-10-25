4708346e124b7ff90398a696ac621f3f8d5f43e9
jonathanduran7 <joniduran624@gmail.com>
2024-10-24 21:46:55 -0300

    add styles for link active

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,22 +1,25 @@
 'use client'
 
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { ThemeContext } from '../context/ThemeContext';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 
 export default function Navbar() {
 
   const { theme, handleThemeChange } = useContext(ThemeContext)
+  const path = usePathname()
 
   return (
     <div className="w-full flex justify-between text-black dark:text-darkForeground">
       <ul className="flex gap-4">
-        <Link href="/" className="cursor-pointer underline font-bold">
+        <Link href="/" className={`cursor-pointer ${path === '/' && 'underline font-bold'}`}>
           home
         </Link>
-        <Link href="/blog" className='cursor-pointer'>
+        <Link href="/blog" className={`cursor-pointer ${path.includes('blog') && 'underline font-bold'}`}>
           blog
         </Link>
       </ul>
