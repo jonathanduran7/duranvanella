c5474eef11a7cc9a66dd45e170201d3649906769
jonathanduran7 <joniduran624@gmail.com>
2024-10-24 21:30:51 -0300

    add light mode

--- app/blog/components/article.tsx
+++ app/blog/components/article.tsx
@@ -7,10 +7,10 @@ interface ArticleCardProps {
 
 export default function ArticleCard({ post }: ArticleCardProps) {
   return (
-    <div className="bg-darkTerciary rounded-md min-h-28 py-3 px-6">
-      <p className="text-gray-soft text-[12px] font-medium">{dayjs(post.date).format('MMMM DD, YYYY')}</p>
-      <p className="text-white text-[20px] font-medium">{post.title}</p>
-      <p className="text-gray-soft text-[14px] mt-2">{post.description}</p>
+    <div className="dark:bg-darkTerciary bg-[#efefef] rounded-md min-h-28 py-3 px-6">
+      <p className="dark:text-gray-soft text-[#979797] text-[12px] font-medium">{dayjs(post.date).format('MMMM DD, YYYY')}</p>
+      <p className="dark:text-white text-[20px] font-medium">{post.title}</p>
+      <p className="dark:text-gray-soft text-[14px] mt-2">{post.description}</p>
     </div>
   );
 }
