98cd1baa84da8d326d12109de432ab7463761eb4
jonathanduran7 <joniduran624@gmail.com>
2024-10-24 21:24:55 -0300

    modify size title

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
       <Navbar />
 
       <div className="mt-5 mb-5">
-        <h1 className="text-4xl">Welcome to my Next.js blog</h1>
+        <h1 className="text-xl">My blog</h1>
       </div>
 
       <div className="flex flex-col gap-5 pb-6">
