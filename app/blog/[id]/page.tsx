9f3dbe9f0fea85f9787fc334a01cc5bcb3bef985
jonathanduran7 <joniduran624@gmail.com>
2024-10-24 22:01:15 -0300

    modify content post

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -12,6 +12,7 @@ export default async function Post({ params }: { params: { id: string } }) {
 
         <div className='mt-5 mb-2'>
           <p className="dark:text-gray-soft text-[#979797] text-[12px] font-medium">{dayjs(postData.date).format('MMMM DD, YYYY')}</p>
+          <h1 className="dark:text-white text-[20px] font-medium">{postData.title}</h1>
         </div>
 
         <div dangerouslySetInnerHTML={{ __html: postData.content }} />
--- app/posts/auth-nestjs.md
+++ app/posts/auth-nestjs.md
@@ -4,8 +4,6 @@ date: "2024-10-22"
 description: "This post will show you how to authenticate with Nestjs and Passport. We will use JWT as the authentication method."
 ---
 
-# Auth with Nestjs 
-
 ### Código
 ```typescript
 function login() {
